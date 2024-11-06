bd5212f1db4e29ecdc2c262912539f140a1a51f9
anzlotte <andres.lotte@gmail.com>
2024-11-06 17:25:30 -0500

    first commit

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ const clientesRoutes = require('./routes/clientes');
 app.set('port',process.env.PORT || 3000);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'));
+app.use(express.static(__dirname + "public"))
 
 //middlewares
 app.use(morgan('dev'));
